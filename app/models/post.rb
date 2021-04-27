9e5d1c608065f7443db6b2a0c2fc389fac8f9345
snghong <sng_hong@outlook.com>
2021-04-28 00:15:27 +0800

    completed micro reddit exercise

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  validates :title, presence: true, length: { minimum: 1 }
+  validates :body, presence: true, length: { minimum: 1 }
+  
+end
